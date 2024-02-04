23789818f4778926081fcd5826b47537d5b6945f
FukushimaRiki <chsgrncc.0209@ezweb.ne.jp>
2024-02-04 19:21:57 +0900

    breezeとVueのインストール完了

---
+++ src/resources/js/app.js
@@ -0,0 +1,16 @@
+// import './bootstrap';
+
+// import Alpine from 'alpinejs';
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
+
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+createApp({
+    components: {
+        ExampleComponent
+    }
+}).mount('#app');
